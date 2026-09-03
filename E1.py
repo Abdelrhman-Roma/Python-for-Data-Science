7e8361d4ed5e5550d00c4f835afae9633e31de35
Abdelrhman <abdelrhman.roma2006@gmail.com>
2026-09-03 06:49:20 +0300

    Add copyright notice to E1
    Added copyright notice and usage restrictions.

--- E1.py
+++ E1.py
@@ -1,3 +1,9 @@
+# Copyright (c) 2026 Abdelrhman Taha
+# All Rights Reserved.
+#
+# This material is part of the Python-for-Data-Science course.
+# Unauthorized reproduction, redistribution, or commercial use is prohibited.
+
 # what we learn in E1 ?
 # 1.how to install VSCode
 # 2.how to install python
